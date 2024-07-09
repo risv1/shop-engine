24d61738f5dda1297151c9737443c49c5f24be1a
risv1 <rishi.aangarai@gmail.com>
2024-07-09 17:53:15 +0530

    refactor: add conn pool

--- src/database/conn.rs
+++ src/database/conn.rs
@@ -1,12 +1,14 @@
 use diesel::pg::PgConnection;
-use diesel::prelude::*;
+use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
 use dotenvy::dotenv;
 use std::env;
 
-pub fn establish_connection() -> PgConnection {
+pub fn establish_connection() -> PooledConnection<ConnectionManager<PgConnection>> {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = Pool::builder().build(manager).expect("Failed to create pool");
+
+    pool.get().expect("Failed to get connection")
 }
